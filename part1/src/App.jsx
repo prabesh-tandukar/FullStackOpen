2fa1e1df25a4e5120f02feea4afde11b20ecaafa
Prabesh <tandukarprabesh@gmail.com>
2024-04-09 22:35:13 +1200

    multi state configuration

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,39 +1,33 @@
 import { useState } from "react";
-
-const Display = (props) => {
-  return <div>{props.counter}</div>;
-};
-
-const Button = (props) => {
-  return <button onClick={props.onClick}>{props.text}</button>;
-};
-
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  console.log("rendering with counter value", counter);
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0,
+  });
 
-  const increaseByOne = () => {
-    console.log("increasing, value before", counter);
-    setCounter(counter + 1);
+  const handleLeftClick = () => {
+    const newClicks = {
+      left: clicks.left + 1,
+      right: clicks.right,
+    };
+    setClicks(newClicks);
   };
 
-  const decreaseByOne = () => {
-    console.log("decreasing, value before", counter);
-    setCounter(counter - 1);
-  };
-
-  const setToZero = () => {
-    console.log("resetting to zero, value before", counter);
-    setCounter(0);
+  const handleRightClick = () => {
+    const newClicks = {
+      left: clicks.left,
+      right: clicks.right + 1,
+    };
+    setClicks(newClicks);
   };
 
   return (
-    <>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text={"plus"} />
-      <Button onClick={decreaseByOne} text={"minus"} />
-      <Button onClick={setToZero} text={"zero"} />
-    </>
+    <div>
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      {clicks.right}
+      <button onClick={handleRightClick}>right</button>
+    </div>
   );
 };
 
