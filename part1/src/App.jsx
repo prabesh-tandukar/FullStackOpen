ca80c7d56e4da34a762e5dcb7e03543c84406e94
Prabesh <tandukarprabesh@gmail.com>
2024-04-11 23:27:31 +1200

    tasks

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,26 +1,57 @@
 import { useState } from "react";
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(" ")};</div>;
+};
+
+const hello = (who) => {
+  return () => {
+    console.log("hello", who);
+  };
+};
+
 const App = () => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
   const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [value, setValue] = useState(10);
 
   const handleLeftClick = () => {
     setAll(allClicks.concat("L"));
     setLeft(left + 1);
+    setTotal(left + right);
   };
 
   const handleRightClick = () => {
     setAll(allClicks.concat("R"));
     setRight(right + 1);
+    setTotal(left + right);
+  };
+
+  const setToValue = (newValue) => () => {
+    console.log("value now", newValue);
+    setValue(newValue);
   };
 
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
+      <Button onClick={handleLeftClick} text="left" />
+      <Button onClick={handleRightClick} text="right" />
       {right}
-      <button onClick={handleRightClick}>right</button>
       <p>{allClicks.join(" ")}</p>
+      <History allClicks={allClicks} />
+      <button onClick={hello("brooo")}>button</button>
+      {value}
+      <button onClick={setToValue(1000)}>thousand</button>
     </div>
   );
 };
