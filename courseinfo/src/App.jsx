3a2abaa03d48803e1df42b0d9408d0180ad0a6e9
Prabesh <tandukarprabesh@gmail.com>
2024-04-18 23:32:56 +1200

    redux

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -22,6 +22,7 @@ const Content = (props) => {
       <Part part={props.part1} exercises={props.exercises1} />
       <Part part={props.part2} exercises={props.exercises2} />
       <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={props.part4} exercises={props.exercises4} />
     </>
   );
 };
@@ -31,7 +32,10 @@ const Total = (props) => {
     <>
       <p>
         Number of exercises{" "}
-        {props.exercises1 + props.exercises2 + props.exercises3}
+        {props.exercises1 +
+          props.exercises2 +
+          props.exercises3 +
+          props.exercises4}
       </p>
     </>
   );
@@ -53,6 +57,11 @@ const App = () => {
         exercises: 14,
         id: 3,
       },
+      {
+        name: "Redux",
+        exercises: 11,
+        id: 4,
+      },
     ],
   };
 
@@ -66,11 +75,14 @@ const App = () => {
         exercises2={course.parts[1].exercises}
         part3={course.parts[2].name}
         exercises3={course.parts[2].exercises}
+        part4={course.parts[3].name}
+        exercises4={course.parts[3].exercises}
       />
       <Total
         exercises1={course.parts[0].exercises}
         exercises2={course.parts[1].exercises}
         exercises3={course.parts[2].exercises}
+        exercises4={course.parts[3].exercises}
       />
     </>
   );
