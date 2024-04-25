d00d31f1d9da42af8cd0f406754ad2cda1ffdbcc
Prabesh <tandukarprabesh@gmail.com>
2024-04-25 23:01:42 +1200

    reduce

--- jsbasic/index.js
+++ jsbasic/index.js
@@ -1,10 +1,5 @@
-const array1 = [1, 2, 3, 4];
+const numbers = [1, 2, 3, 4, 5];
 
-const initialValue = 0;
-const sumWithInitial = array1.reduce(
-  (accumulator, currentValue) => accumulator + currentValue,
-  initialValue
-);
+const sum = numbers.reduce((a, b) => a + b);
 
-console.log(sumWithInitial);
-//
+console.log(sum);
